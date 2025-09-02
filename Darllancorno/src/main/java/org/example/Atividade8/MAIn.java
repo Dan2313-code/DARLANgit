487b1e47ddfb35be06a5e7bcd5ad605475762f87
Dan2313-code <darlan.s.pereira@ba.estudante.senai.br>
2025-09-02 10:17:28 -0300

    Delete Darllancorno/src/main/java/org/example/Atividade8 directory

--- Darllancorno/src/main/java/org/example/Atividade8/MAIn.java
+++
@@ -1,8 +0,0 @@
-package org.example.Atividade8;
-
-public class MAIn  {
-    public static void main(String[] args) {
-        Motoboy motoboy = new Motoboy("Darllan", "01/01/2000", Sexo.MASCULINO, Setor.Operacoes, "ABC-1234");
-        System.out.println(motoboy);
-    }
-}
